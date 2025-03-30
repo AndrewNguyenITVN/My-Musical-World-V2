5a0e7f8440d13f045aa3cfef8890b017d1a25a5c
Nick <nick170104@gmail.com>
2025-03-30 18:34:14 +0700

    Update verify email

--- verify.php
+++ verify.php
@@ -1,63 +1,58 @@
 <?php
-
-    session_start();
-    include('connection.php');
-             
-	if(isset($_SESSION['email_address']) && isset($_SESSION['activation_code']) ){
-	
-    $email_address = mysqli_escape_string($conn,$_SESSION['email_address']); // Set email variable
-    $activation_code = mysqli_escape_string($conn,$_SESSION['activation_code']); // Set hash variable
-                 
-    $search = mysqli_query($conn,"SELECT email_address, activation_code, confirm_status FROM user WHERE email_address = 
-        '$email_address' AND activation_code = '$activation_code' AND confirm_status ='0' ") or die(mysqli_error($conn)); 
-    $match  = mysqli_num_rows($search);
-                 
-    if($match > 0){
-        // We have a match, activate the account
-        $sql = "SELECT * FROM user WHERE email_address = '$email_address' ";
-        $result = mysqli_query($conn,$sql);
-        $row = mysqli_fetch_array($result);
-        $username = $row['username'];
-        mysqli_query($conn,"UPDATE user SET confirm_status='1' WHERE email_address='".$email_address."' AND activation_code='".$activation_code."' AND confirm_status='0'") or die(mysqli_error($conn));
-        echo '<script type="text/javascript">
-            setTimeout(function () { 
-                Swal.fire({
-                    title: "Success",
-                    type: "success",
-                    text: "Thank you '.$username.' your account has been activated successfully"
-                }).then(function(){
-                    window.location.href = "index.php"; 
-                });
-            }, 500);
-        </script>';
-    }else{
-        // No match -> invalid url or account has already been activated.
-        echo '<script type="text/javascript">';
-        echo 'setTimeout(function () { Swal.fire("Oops...","The url is either invalid or you have already activated your account.","error");';
-        echo '}, 500);</script>';
-    }
-}                 
-else{
-    // Invalid approach
-    echo '<script type="text/javascript">';
-    echo 'setTimeout(function () { Swal.fire("Oops...", "Invalid approach please use the link that has been sent to your email.","error");';
-    echo '}, 500);</script>';
-}
+include('connection.php');
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title></title>
+    <title>Verify Account</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="KEERTHANA KUTEERA LOGO-BLACK-01.png" type="image/png">
-  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@7.28.11/dist/sweetalert2.min.css"> -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
+
 <body>
-<!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/sweetalert2@7.28.11/dist/sweetalert2.min.js"></script> -->
+
+    <?php
+    if (isset($_GET['email_address']) && isset($_GET['activation_code'])) {
+        $email_address = mysqli_real_escape_string($conn, $_GET['email_address']);
+        $activation_code = mysqli_real_escape_string($conn, $_GET['activation_code']);
+
+        $search = mysqli_query($conn, "SELECT email_address, activation_code, confirm_status, username FROM user WHERE email_address = '$email_address' AND activation_code = '$activation_code' AND confirm_status = '0'");
+        if (mysqli_num_rows($search) > 0) {
+            $row = mysqli_fetch_assoc($search);
+            $username = $row['username'];
+            mysqli_query($conn, "UPDATE user SET confirm_status = '1' WHERE email_address = '$email_address'");
+            echo "<script>
+            setTimeout(function () {
+                Swal.fire({
+                    title: 'Success',
+                    icon: 'success',
+                    text: 'Thank you $username, your account has been activated successfully!'
+                }).then(() => {
+                    window.location.href = 'index.php';
+                });
+            }, 500);
+        </script>";
+        } else {
+            echo "<script>
+            setTimeout(function () {
+                Swal.fire('Oops...', 'The URL is either invalid or the account has already been activated.', 'error');
+            }, 500);
+        </script>";
+        }
+    } else {
+        echo "<script>
+        setTimeout(function () {
+            Swal.fire('Oops...', 'Invalid approach. Please use the link that was sent to your email.', 'error');
+        }, 500);
+    </script>";
+    }
+    ?>
+
 </body>
+
 </html>
